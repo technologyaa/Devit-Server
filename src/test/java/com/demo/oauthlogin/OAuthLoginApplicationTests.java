2b102e62378dbaa500212e5851b78623078dfbf0
huni0i <huni0i@naver.com>
2025-09-02 23:32:37 +0900

    Add files via upload

---
+++ src/test/java/com/demo/oauthlogin/OAuthLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.oauthlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
