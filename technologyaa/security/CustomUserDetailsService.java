004ff38faf2aeb56446c919aba1a3b46c61b5387
박준석 <131576408+azusa0129@users.noreply.github.com>
2025-10-27 20:55:22 +0900

    Revert "feat: merge Google OAuth and JWT auth code"

---
+++ technologyaa/security/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.demo.technologyaa.security;
+
+import com.demo.technologyaa.user.User;
+import com.demo.technologyaa.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.*;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                List.of(new SimpleGrantedAuthority(user.getRole()))
+        );
+    }
+}
