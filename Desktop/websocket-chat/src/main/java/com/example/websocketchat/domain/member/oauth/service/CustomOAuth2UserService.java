4961d339bc0eb75409d1300c9c7b1ce01ca237f0
huni0i <huni0i@naver.com>
2025-10-28 11:29:13 +0900

    chore: CustomOAuth2UserService 임시 비활성화

--- Desktop/websocket-chat/src/main/java/com/example/websocketchat/domain/member/oauth/service/CustomOAuth2UserService.java
+++ Desktop/websocket-chat/src/main/java/com/example/websocketchat/domain/member/oauth/service/CustomOAuth2UserService.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 
-@Service
+// @Service
 public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
 
     private final UserRepository userRepository;
