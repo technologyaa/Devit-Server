50fe00b7abcc34f8513fb29bb3749472702824f0
huni0i <huni0i@naver.com>
2025-10-28 11:28:42 +0900

    fix: ProjectRequest에 NoArgsConstructor 추가

--- Desktop/websocket-chat/src/main/java/com/example/websocketchat/domain/project/dto/ProjectRequest.java
+++ Desktop/websocket-chat/src/main/java/com/example/websocketchat/domain/project/dto/ProjectRequest.java
@@ -1,11 +1,13 @@
 package com.example.websocketchat.domain.project.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import com.example.websocketchat.domain.project.entity.Major;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class ProjectRequest {
     private String title;
     private String content;
