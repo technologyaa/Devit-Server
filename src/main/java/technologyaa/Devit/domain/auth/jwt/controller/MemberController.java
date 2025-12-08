94f837528a42d9b4aacaf5e17ecea1c42aec9194
ParkJunSeok <jdksla0129@gmail.com>
2025-12-08 20:53:44 +0900

    fix:memberService

--- src/main/java/technologyaa/Devit/domain/auth/jwt/controller/MemberController.java
+++ src/main/java/technologyaa/Devit/domain/auth/jwt/controller/MemberController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import technologyaa.Devit.domain.auth.jwt.dto.SignInRequest;
+import technologyaa.Devit.domain.auth.jwt.dto.SignOutRequest;
 import technologyaa.Devit.domain.auth.jwt.dto.SignUpRequest;
 import technologyaa.Devit.domain.auth.jwt.service.MemberService;
 import technologyaa.Devit.domain.common.APIResponse;
@@ -36,6 +37,12 @@ public class MemberController {
     @PutMapping("/profile/image")
     public APIResponse<?> uploadProfileImage(@RequestParam("file")MultipartFile file) {
         return memberService.uploadProfileImage(file);
+    }
+
+    @Operation(summary = "아이디 중복 확인", description = "아이디가 기존에 있는 아이디인지 확인합니다.")
+    @PostMapping("/check")
+    public boolean checkandUsername(@RequestBody SignOutRequest request) {
+        return memberService.checkUsername(request);
     }
 }
 
