16a55e42e3dea5eed72ba7565e2623befec73cae
huni0i <huni0i@naver.com>
2025-12-18 02:06:48 +0900

    fix: 채팅 API CORS 오류 수정
    - SecurityConfig에 CORS 설정 명시적으로 적용
    - ChatRoomController, ChatMessageController에 @CrossOrigin 어노테이션 추가
    - corsConfigurationSource() 메서드 분리하여 재사용 가능하게 변경

--- src/main/java/technologyaa/Devit/domain/websocketchat/controller/ChatMessageController.java
+++ src/main/java/technologyaa/Devit/domain/websocketchat/controller/ChatMessageController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/chat/messages")
+@CrossOrigin(origins = {"http://localhost:5173", "https://devit.run"}, allowCredentials = "true")
 public class ChatMessageController {
 
     private final ChatMessageService chatMessageService;
